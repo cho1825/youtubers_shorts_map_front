c06e6dbc4d6393725ea7fcdbc0e5e1a530057b02
cho1825 <devcho1825@gmail.com>
2025-01-06 12:18:12 +0900

    publish - 인디케이터 생성

---
+++ src/components/main/jsx/Indicator.jsx
@@ -0,0 +1,12 @@
+import "../css/indicator.css"
+
+const Indicator = () => {
+
+    return (
+        <div
+        className="zoom-indicator-fixed"
+        onClick={onZoomClick}
+        ></div>
+    );
+
+}
