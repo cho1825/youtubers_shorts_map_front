451d3dc3e29b03c5addeeec19dff8167622307d0
cho1825 <devcho1825@gmail.com>
2025-01-07 22:51:19 +0900

    setting - 접근 ip 관련 설정

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // 0.0.0.0으로 바인딩하여 네트워크 전체에서 접근 가능
+    port: 5173  // 필요에 따라 포트 설정
+  }
 })
