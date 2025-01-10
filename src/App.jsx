cb4501f6db74a980e3598ef8ce45584b2074ad77
cho1825 <devcho1825@gmail.com>
2025-01-10 22:18:47 +0900

    feat - 라우터 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 // src/App.jsx
 import './index.css';
 import MainPage from './pages/MainPage.jsx'
+import {Route, Routes} from "react-router-dom";
+import NewPage from "./pages/NewPage.jsx";
 
 
 function App() {
     return (
         <div className="App">
-            <MainPage />
+            <Routes>
+                <Route path="/" element={<MainPage />}/>
+                <Route path="/test" element={<NewPage/>}/>
+            </Routes>
         </div>
     );
 }
