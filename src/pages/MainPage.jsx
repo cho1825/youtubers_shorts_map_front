cbefd59359baa3a4fca1b5296ce34081c7f4929b
cho1825 <devcho1825@gmail.com>
2025-01-13 22:25:16 +0900

    feat - 공통 함수 프롭스 생성

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,7 +1,7 @@
 import "./MainPage.css";
 import HeaderSection from "../components/common/jsx/HeaderSection.jsx"
-import MapSection from "../components/main/jsx/MapSection.jsx";
-import MessageSection from "../components/main/jsx/MessageSection.jsx";
+import MapSection from "../components/main/jsx/map/MapSection.jsx";
+import MessageSection from "../components/main/jsx/explain/TextSection.jsx";
 
 import {useState} from "react";
 
@@ -13,6 +13,26 @@ const MainPage = () => {
     const personClick = () => {
         setShowMessageSection(false);
     }
+
+    const getMapDataByApi = async (regionCode,youtuberNm,category) => {
+
+        try{
+                const response = await fetch(`http://192.168.0.177:8080/api/makers?regionCode=${regionCode}&youtuberNm=${youtuberNm}&category=${category}`,{
+                method: "GET",
+            });
+            if (!response.ok) {
+                throw new Error("Api 요청 실패: " + response.statusText);
+            }
+
+            const placeDtoList = await response.json();
+            setMapData(placeDtoList);
+        }catch (error){
+            console.error("API 요청 중 에러 발생:", error);
+            setMapData(null)
+        }
+
+    }
+
 
     return (
         <div className="container">
@@ -27,11 +47,13 @@ const MainPage = () => {
                 showMessageSection={showMessageSection}
                 setShowMessageSection={setShowMessageSection}
                 mapData={mapData}
+                getMapDataByApi={getMapDataByApi}
             />
             {showMessageSection &&
                 <MessageSection
                     onZoomClick={personClick}
                     setMapData={setMapData}
+                    getMapDataByApi={getMapDataByApi}
                 />
             }
 
