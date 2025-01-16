2c60f369aa88238e92801d3370047c2e7db245f9
cho1825 <devcho1825@gmail.com>
2025-01-16 23:15:31 +0900

    feat - 가게 정보 표시 전역 상태

---
+++ src/store/markerInfo.js
@@ -0,0 +1,37 @@
+import {create} from "zustand";
+
+const useMarkerInfoStore = create((set) => ({
+
+    id: null,
+    name: null,
+    categoryGroupName: null,
+    categoryName: null,
+    description: null,
+    lotAddress: null,
+    roadAddress: null,
+    youtuberNm: null,
+    videoId: null,
+    videoUrl: null,
+    latitude: null,
+    longitude: null,
+    phoneNumber: null,
+    isClick: false,
+
+    setId:(id) => set({id}),
+    setName:(name) => set({name}),
+    setCategoryGroupName:(categoryGroupName) => set({categoryGroupName}),
+    setCategoryName:(categoryName) => set({categoryName}),
+    setDescription:(description) => set({description}),
+    setLotAddress:(lotAddress) => set({lotAddress}),
+    setRoadAddress:(roadAddress) => set({roadAddress}),
+    setYoutuberNm:(youtuberNm) => set({youtuberNm}),
+    setVideoId:(videoId) => set({videoId}),
+    setVideoUrl:(videoUrl) => set({videoUrl}),
+    setLatitude:(latitude) => set({latitude}),
+    setLongitude:(longitude) => set({longitude}),
+    setPhoneNumber: (phoneNumber) => set({phoneNumber}),
+    setIsClick: (isClick) => set({isClick}),
+    setAll: (newState) => set((state) => ({ ...state, ...newState })),
+}));
+
+export default useMarkerInfoStore;
