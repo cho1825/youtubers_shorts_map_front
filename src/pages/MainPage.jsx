edfdbad11941073bedba048474f675e58e66421a
cho1825 <devcho1825@gmail.com>
2025-01-17 15:07:29 +0900

    fix - mapInfo 관련 스테이트 및 함수 전역상태 관리 추가

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,59 +1,30 @@
 import "./MainPage.css";
 import HeaderSection from "../components/common/jsx/HeaderSection.jsx"
 import MapSection from "../components/main/jsx/map/MapSection.jsx";
-import MessageSection from "../components/main/jsx/explain/TextSection.jsx";
+import TextSection from "../components/main/jsx/explain/TextSection.jsx";
 
 import {useState} from "react";
 
 const MainPage = () => {
 
-    const [showMessageSection, setShowMessageSection] = useState(true);
-    const [mapData, setMapData] = useState(null);
+    const [showTextSection, setShowTextSection] = useState(true);
 
     const personClick = () => {
-        setShowMessageSection(false);
+        setShowTextSection(false);
     }
-
-    const getMapDataByApi = async (regionCode,youtuberNm,category) => {
-
-        try{
-                const response = await fetch(`http://192.168.0.177:8080/api/makers?regionCode=${regionCode}&youtuberNm=${youtuberNm}&category=${category}`,{
-                method: "GET",
-            });
-            if (!response.ok) {
-                throw new Error("Api 요청 실패: " + response.statusText);
-            }
-
-            const placeDtoList = await response.json();
-            setMapData(placeDtoList);
-        }catch (error){
-            console.error("API 요청 중 에러 발생:", error);
-            setMapData(null)
-        }
-
-    }
-
-
     return (
         <div className="container">
 
             <HeaderSection
-                showMessageSection={showMessageSection}
-                setShowMessageSection={setShowMessageSection}
-                mapData={mapData}
-                setMapData={setMapData}
+                showTextSection={showTextSection}
+                setShowTextSection={setShowTextSection}
             />
             <MapSection
-                showMessageSection={showMessageSection}
-                setShowMessageSection={setShowMessageSection}
-                mapData={mapData}
-                getMapDataByApi={getMapDataByApi}
+                showTextSection={showTextSection}
             />
-            {showMessageSection &&
-                <MessageSection
+            {showTextSection &&
+                <TextSection
                     onZoomClick={personClick}
-                    setMapData={setMapData}
-                    getMapDataByApi={getMapDataByApi}
                 />
             }
 
