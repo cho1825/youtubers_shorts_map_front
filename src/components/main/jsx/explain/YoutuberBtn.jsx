9147f694ed4f3a16ad5c063a595c0f31a09be0c0
cho1825 <devcho1825@gmail.com>
2025-01-19 23:32:34 +0900

    feat - 버튼 애니매이션 추가

--- src/components/main/jsx/explain/YoutuberBtn.jsx
+++ src/components/main/jsx/explain/YoutuberBtn.jsx
@@ -1,8 +1,11 @@
+import { motion, useAnimation } from "framer-motion";
+
 import "../../css/YoutuberBtn.css"
-import useMapInfoStore from "../../../../store/mapInfo.js";
+import useMapInfoStore from "../../../../store/useMapInfoStore.js";
+import {useEffect} from "react";
 
 const YoutuberBtn = ({name, count, onZoomClick, regionCode, youtuberNm}) => {
-
+    const controls = useAnimation(); // 애니메이션 컨트롤러
     const setYoutuberNm = useMapInfoStore((state) => state.setYoutuberNm);
     const setRegionCode = useMapInfoStore((state) => state.setRegionCode);
     const {getMapDataByApi} = useMapInfoStore();
@@ -20,10 +23,26 @@ const YoutuberBtn = ({name, count, onZoomClick, regionCode, youtuberNm}) => {
         }
     };
 
+    // 버튼 애니메이션 반복 실행
+    useEffect(() => {
+        controls.start({
+            scale: [1, 1.05, 1],
+            transition: {
+                duration: 2, // 한 번 커졌다 작아지는 데 걸리는 시간
+                repeat: Infinity, // 무한 반복
+                ease: "easeInOut", // 부드러운 움직임
+            },
+        });
+    }, [controls]);
+
     return (
-        <button className="Btn"
+        <motion.button
+                className="Btn"
                 onClick={handleClick}
                 onTouchStart={handleClick}
+                animate={controls} // 애니메이션 컨트롤 연결
+                whileHover={{ scale: 1.2 }} // 호버 시 더 커지는 효과
+                whileTap={{ scale: 0.95 }} // 클릭 시 작아지는 효과
         >
             <span className="leftContainer">
             <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" fill="#fff"><path
@@ -31,9 +50,9 @@ const YoutuberBtn = ({name, count, onZoomClick, regionCode, youtuberNm}) => {
             <span className="like">{name}</span>
             </span>
             <span className="likeCount">
-            {count}
+            {count} 개
           </span>
-        </button>
+        </motion.button>
     )
 }
 
