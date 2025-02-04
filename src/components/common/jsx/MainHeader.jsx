5dafa6167a92190c9ca164e1ef567298584713cb
cho1825 <devcho1825@gmail.com>
2025-02-04 16:11:36 +0900

    fix - 헤더 랜더링 문제 수정

--- src/components/common/jsx/MainHeader.jsx
+++ src/components/common/jsx/MainHeader.jsx
@@ -2,36 +2,48 @@ import "/src/components/common/css/MainHeader.css"
 import {useNavigate} from "react-router-dom";
 import useMapInfoStore from "../../../store/useMapInfoStore.js";
 import useShowTextSectionStore from "../../../store/useShowTextSectionStore.js";
+import {useCallback, useEffect, useState, useTransition} from "react";
 
 
 const MainHeader = () => {
     const navigate = useNavigate();
     const {setMapData} = useMapInfoStore();
-    const { changeShowTextSectionState } = useShowTextSectionStore();
-    const { showTextSection } = useShowTextSectionStore();
-    const { setShowTextSection } = useShowTextSectionStore();
+    const { showTextSection, setShowTextSection, changeShowTextSectionState } = useShowTextSectionStore();
 
+    const [isLoading, setIsLoading] = useState(true);
+    const [imageSrc, setImageSrc] = useState(showTextSection ? "/img/DaejeonMap2-logo-black.png" : "/img/48.png");
+    const [isPending, startTransition] = useTransition();
+
+
+    useEffect(() => {
+        setIsLoading(true);
+        const img = new Image();
+        img.src = showTextSection ? "/img/DaejeonMap2-logo-black.png" : "/img/48.png";
+        img.onload = () => {
+            startTransition(() => {
+                setImageSrc(img.src);
+                setIsLoading(false);
+            });
+        };
+    }, [showTextSection]);
+
+    const handleClick = useCallback(() => {
+        changeShowTextSectionState(navigate, setMapData);
+        setShowTextSection(true);
+    }, [changeShowTextSectionState, navigate, setMapData, setShowTextSection]);
 
     return (
         <div className="main-header">
-            <div
-                className="image-container"
-                onClick={() => {
-                    changeShowTextSectionState(navigate, setMapData);
-                    setShowTextSection(true);
-                }}
-            >
+            <div className="image-container" onClick={handleClick}>
                 <img
-                    src={showTextSection ? "/img/DaejeonMap2-logo-black.png" : "/img/back_btnn.png"}
+                    src={imageSrc}
                     alt="logo"
-                    className={showTextSection ? "logo-image" : "backBtn-image"}
+                    className={isLoading ? "loading" : "loaded"}
                 />
             </div>
-            {/*<div className="menu-icon">*/}
-            {/*    <img src="/img/ham.png" alt="logo" className="menu-image" />*/}
-            {/*</div>*/}
         </div>
     );
 }
+
 
 export default MainHeader;
